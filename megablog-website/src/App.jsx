aabb7374f12b0c69c7a498e1cffee8561366b06d
Sayan <sayandutta84314@gamil.com>
2024-12-09 21:27:12 +0530

    env file setup

--- megablog-website/src/App.jsx
+++ megablog-website/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 
 function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
       <h1>Hey bro, I am here</h1>
     </>
   );
 }
-
+  
 export default App;
